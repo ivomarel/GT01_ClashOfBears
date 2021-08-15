d130c19c6edfb1c5388282e9ea6a998b649a6b87
gavin <a294713590@gmail.com>
2021-08-15 01:37:44 -0700

    add a tanker script

---
+++ Assets/Scripts/Gameplay/Tanker.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tanker : Unit
+{
+    public LintVector3 spawnOffset;
+    public Arrow arrowPrefab;
+
+    protected override void Attack()
+    {
+        anim.SetTrigger("Attack");
+        anim.SetFloat("Speed", 0);
+        Linvoke(SpawnArrow, delayToDoDamageOnAttack);
+    }
+
+    private void SpawnArrow()
+    {
+        Arrow arrowClone = Instantiate(arrowPrefab);
+        arrowClone.lintTransform.position = lintTransform.position + spawnOffset;
+        arrowClone.lintTransform.radians = lintTransform.radians;
+    }
+}
